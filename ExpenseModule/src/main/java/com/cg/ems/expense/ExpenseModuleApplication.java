0d88e702e01987869c66ecf9f1218da748050cb1
Anurag Kumar <agresiveanurag4one@gmail.com>
2019-11-09 20:16:19 +0530

    first commit

---
+++ ExpenseModule/src/main/java/com/cg/ems/expense/ExpenseModuleApplication.java
@@ -0,0 +1,13 @@
+package com.cg.ems.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseModuleApplication.class, args);
+	}
+
+}
